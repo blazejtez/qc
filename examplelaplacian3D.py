55d49979c18c96ab1915b05631b1af4b6b4204b8
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-12 09:22:22 +0100

    added wolframscript for plotting volumetric data (e.g. wave functions for a single particle)

--- examplelaplacian3D.py
+++ examplelaplacian3D.py
@@ -2,17 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import torch
-import numpy as np
-import time
-tic = time.perf_counter()
-a = np.random.randn(1024,1024,1024).astype(np.float32)
-toc = time.perf_counter()
-
-print(f"Generation time: {toc-tic:4f} seconds") 
-tic = time.perf_counter()
-a += 8*a
-toc = time.perf_counter()
-print(f"Addition time: {toc-tic:4f} seconds") 
 
 start = torch.cuda.Event(enable_timing=True)
 end = torch.cuda.Event(enable_timing=True)
@@ -23,18 +12,19 @@ end.record()
 print(start.elapsed_time(end))
 start.record()
 x = torch.randn(1024, 1024, 1024, dtype=torch.float32, device=device0)
-#y = torch.randn(1024, 1024, 1024, dtype=torch.float32, device=device0)
+y = torch.randn(1024, 1024, 1024, dtype=torch.float32, device=device0)
 end.record()
 torch.cuda.synchronize()
 print(start.elapsed_time(end))
 #x = x.to(device0)
 #y = y.to(device0)
 start.record()
-x += 8*x 
+z = x + y
+v = x + y
 end.record()
 torch.cuda.synchronize()
 print(start.elapsed_time(end))
 start.record()
-x_cpu = x.to("cpu", torch.float32)
+z_cpu = z.to("cpu", torch.float32)
 end.record()
 print(start.elapsed_time(end))
