0c9fb89b36a414ac56b02e20d38c8777ff041b5f
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-12 09:34:41 +0100

    added laplacian3Dnumpy

---
+++ laplacian3Dnumpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+class Laplacian3D:
+
+    def __init__(self, shape : tuple):
+        self.shape = shape
+
+    def matvec(vec : np.ndarray) -> np.ndarray:
+        vec =  fs
+        
