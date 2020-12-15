55bc01622177e4f6c00afafbdb93f990af24cbb8
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-15 18:06:38 +0100

    earlier uncommited changes

--- .SpaceVim.d/autoload/myspacevim.vim
+++ .SpaceVim.d/autoload/myspacevim.vim
@@ -4,6 +4,9 @@ function! myspacevim#before() abort
   autocmd BufNewFile,BufRead *.wl set syntax=wl
   autocmd BufNewFile,BufRead *.wls set syntax=wl
   autocmd BufNewFile,BufRead *.m set syntax=wl
+  nnoremap <leader>gd :Gvdiff<CR>
+  nnoremap gdh :diffget //2<CR>
+  nnoremap gdl :diffget //3<CR>
 endfunction
 
 function! myspacevim#after() abort
---
+++ hydrogen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('./Hydrogen-Radial-Wavefunctions')
+import RadialPsi
+import numpy as np
+from scipy.special import genlaguerre
+
+class RadialHydrogen:
+
+    def __init__(self, n, l):
+        
+
+if __name__ == "__main__":
+
+     print(Radial(2,0,1.)) 
+     print(1./(2**.5)*.5*np.exp(-.5))
+     
+
+print(2*np.exp(-1))
