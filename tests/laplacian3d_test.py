3a4fdc7c5f8af9091fba3f1e20ee4057319440a3
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-23 13:42:17 +0100

    added unittest for laplacian3d

--- tests/laplacian3d_test.py
+++ tests/laplacian3d_test.py
@@ -1,22 +1,23 @@
 import unittest
-import qc
+from qc.laplacian3d import *
+import numpy as np
 class MainTest(unittest.TestCase):
 
     def test(self):
 
-        s = qc.laplacian3d.Stencils3D()
+        s = Stencils3D()
 
         shape=(10,10,10)
 
-        lap = qc.laplacian3d.Laplacian3D(shape,s.stencil2)
+        lap = Laplacian3D(shape,s.stencil2)
 
         cube = np.random.randn(*shape).astype(np.float32)
 
-        cube_out = lap_matcube(cube)
+        cube_out = lap.matcube(cube)
         
         cube_out_numba = lap.matcube_numba(cube)
         
-        self.assertAlmostEqual(np.sum((cube_out-cube_out_numbe)**2), 0.0)
+        self.assertAlmostEqual(np.sum((cube_out-cube_out_numba)**2), 0.0)
 
 if __name__ == "__main__":
 
