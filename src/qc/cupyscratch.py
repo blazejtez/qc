badbb37f759aacd0c8b75ceb63ced0ed96cdc3eb
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-30 18:18:46 +0100

    experiments on using cupy, significant acceleration observed while generating random normal samples in large amounts

---
+++ src/qc/cupyscratch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cupy as cp
+import numpy as np
+import time 
+xlen = 1000
+ylen = 6
+pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
+cp.cuda.set_allocator(pool.malloc)
+start_event = cp.cuda.stream.Event()
+stop_event = cp.cuda.stream.Event()
+stream = cp.cuda.stream.Stream()
+with stream:
+    start_event.record()
+    x = cp.random.randn(xlen**3,ylen,dtype=cp.float32)
+    z = cp.transpose(x).dot(x)*xlen**-3.
+    stop_event.record()
+    stop_event.synchronize()
+    print(z)
+    print(f"{cp.cuda.get_elapsed_time(start_event,stop_event)*1e-3}")
+#  tic = time.time()
+#  x_cpu = np.random.randn(xlen**3,ylen).astype(np.float32)
+#  z_cpu = np.transpose(x_cpu).dot(x_cpu)*xlen**-3.
+#  print(z_cpu)
+#  toc = time.time()
+#  print(f"{toc-tic}")
