e0fb97bff477bcab7ebc4b75f41b907c650905ba
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2020-12-30 18:18:01 +0100

    timings on lobpcg, to slow, arount 16 sec per iteration

--- src/qc/lobpcg.py
+++ src/qc/lobpcg.py
@@ -18,56 +18,50 @@ class HydrogenLOBPCG:
 
         STS = np.transpose(S).dot(S)
         Ddiag = np.diag(STS)**(-.5)
-        print(Ddiag)
         D = np.diag(Ddiag)
         Ddiag = Ddiag[..., np.newaxis]
-
+        print(Ddiag.shape)
         DSTSD = Ddiag * STS * Ddiag.T
-        print(np.linalg.cond(DSTSD))
 
         R = scipy.linalg.cholesky(DSTSD)
 
         invR = scipy.linalg.lapack.dtrtri(R)[0]  # returns np.inv(R.T)
-        print(invR)
         AS = self.h.operate_vec(S)
         STAS = np.transpose(S).dot(AS)
-        print(np.diag(STAS))
         AUX = invR.dot(Ddiag * STAS * Ddiag.T).dot(np.transpose(invR))
-        print(AUX)
         w, v = scipy.linalg.eigh(AUX)
-        print(w)
-        print(v)
         C = (Ddiag * np.transpose(invR)).dot(v)
         return C, np.diag(w)
 
     def lobpcgK(self, X: np.ndarray, tau: float):
         C, Theta = self.RayleighRitz(X)
-        print(Theta)
         diagTheta = np.diag(Theta)
         diagTheta = diagTheta[..., np.newaxis]
         nx = np.size(X, 1)
         m = np.size(X, 0)
-        print(diagTheta.T)
         X = X.dot(C)
         R = self.h.operate_vec(X) - X * diagTheta.T
         S = np.empty((m, 2 * nx))
         S[:, :nx] = X
         S[:, nx:] = R
-        print(S)
-        print('start loop')
         for i in range(20):
+            tic = time.time()
             [C, Theta] = self.RayleighRitz(S)
             X = S.dot(C[:, :nx])
             diagTheta = np.diag(Theta)
             print(f'eigenvalue: {np.min(diagTheta)}')
             diagTheta = diagTheta[..., np.newaxis]
+            tic1 = time.time()
             R = self.h.operate_vec(X) - X * diagTheta.T[:, :nx]
+            toc1 = time.time()
+            print(f'time elapsed for R: {toc1-tic1} sec.')
             P = S[:, nx:].dot(C[nx:, :nx])
             S = np.empty((m, 2 * nx + np.size(P, 1)))
             S[:, :nx] = X
             S[:, nx:2 * nx] = R
             S[:, 2 * nx:] = P
-
+            toc = time.time()
+            print(f'time elapsed for one iteration: {toc-tic} sec.')
 
 if __name__ == "__main__":
     HYDROGEN_RADIUS = 30
