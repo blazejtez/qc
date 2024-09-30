55d287b00564fc8b063decfe677b3f7e0917dc52
Blazej Tez <blatez@student.pg.edu.pl>
2024-09-30 02:41:37 +0200

    hydrogen_numerical.py renamed for better understanding of its function

--- src/qc/solver/ground_state_adam.py
+++ src/qc/solver/ground_state_adam.py
@@ -8,50 +8,6 @@ import Praktyki.cut_box_3D as box
 import qc.hamiltonian.hamiltonian as H
 import qc.data.raster as raster
 
-HYDROGEN_RADIUS = 10.
-ALPHA = 0.28294212105225837470023780155114
-
-interval = P.closed(-HYDROGEN_RADIUS, HYDROGEN_RADIUS)
-box_ = box.box3D(interval, interval, interval)
-r = raster.Raster(10)
-xl, yl, zl = r.box_linspaces(box_)
-N = len(xl) * len(yl) * len(zl)
-print("N:", N)
-
-h = H.HamiltonianOperatorCuPy(xl, yl, zl, extent=HYDROGEN_RADIUS)
-
-# tutaj początkowym wektorem jest losowy wektor znormalizowany. To dajmy funkcję GTO w to miejsce.
-# v_init = cp.random.randn(*(len(xl) * len(yl) * len(zl), 1), dtype=cp.float32)
-#
-# v_init = v_init / cp.linalg.norm(v_init)
-
-X, Y, Z = cp.meshgrid(cp.array(xl), cp.array(yl), cp.array(zl))
-gaussian_orbital = cp.exp(-ALPHA*(X**2 + Y**2 + Z**2), dtype=cp.float32)
-norm_factor = cp.sum(gaussian_orbital ** 2) * h.h ** 3
-gaussian_orbital /= cp.sqrt(norm_factor)
-
-#
-# norm_factor = cp.sum(gaussian_orbital ** 2) * h.h ** 3
-# gaussian_orbital /= cp.sqrt(norm_factor)
-
-v_init = cp.reshape(gaussian_orbital, (len(X) * len(Y) * len(Z), 1))
-
-# Adam parameters
-beta1 = 0.9  # Exponential decay rate for the first moment
-beta2 = 0.999  # Exponential decay rate for the second moment
-epsilon = 1e-10  # Small constant to prevent division by zero
-learning_rate = 1e-5  # Step size
-max_iters = 1000
-tolerance = 1e-4
-
-# Initialize first moment (m) and second moment (v)
-m = cp.zeros_like(v_init)
-v = cp.zeros_like(v_init)
-
-# Initialize time step
-t = 0
-
-
 # Gradient Descent to Minimize Rayleigh Quotient with Adam
 def rayleigh_quotient(v, h):
     num = cp.dot(v.T, h.matvec(v))  # Numerator: v^H * H * v
@@ -66,7 +22,38 @@ def gradient(v, h):
     grad = (Hv - rayleigh_quotient_value * v) / denom  # Gradient of Rayleigh quotient
     return grad, rayleigh_quotient_value
 
-if __name__ == "__main__":
+def adam(max_iters=1000):
+    HYDROGEN_RADIUS = 10.
+    ALPHA = 0.28294212105225837470023780155114
+
+    interval = P.closed(-HYDROGEN_RADIUS, HYDROGEN_RADIUS)
+    box_ = box.box3D(interval, interval, interval)
+    r = raster.Raster(10)
+    xl, yl, zl = r.box_linspaces(box_)
+    N = len(xl) * len(yl) * len(zl)
+
+    h = H.HamiltonianOperatorCuPy(xl, yl, zl, extent=HYDROGEN_RADIUS)
+
+    X, Y, Z = cp.meshgrid(cp.array(xl), cp.array(yl), cp.array(zl))
+    gaussian_orbital = cp.exp(-ALPHA * (X ** 2 + Y ** 2 + Z ** 2), dtype=cp.float32)
+    norm_factor = cp.sum(gaussian_orbital ** 2) * h.h ** 3
+    gaussian_orbital /= cp.sqrt(norm_factor)
+
+    v_init = cp.reshape(gaussian_orbital, (len(X) * len(Y) * len(Z), 1))
+
+    # Adam parameters
+    beta1 = 0.9  # Exponential decay rate for the first moment
+    beta2 = 0.999  # Exponential decay rate for the second moment
+    epsilon = 1e-10  # Small constant to prevent division by zero
+    learning_rate = 1e-5  # Step size
+    tolerance = 1e-4
+
+    # Initialize first moment (m) and second moment (v)
+    m = cp.zeros_like(v_init)
+    v = cp.zeros_like(v_init)
+
+    # Initialize time step
+    t = 0
     # Optimization loop with Adam
     v_curr = v_init.copy()
 
@@ -100,9 +87,13 @@ if __name__ == "__main__":
         # Update the current vector
         v_curr = v_new
 
+    # Calculate the final eigenvalue
+    eigenvalue = rayleigh_quotient(v_curr, h)
+    # Print the calculated eigenvalue
+    print("Calculated Eigenvalue:", eigenvalue)
+    return eigenvalue, v_curr
 
-# Calculate the final eigenvalue
-eigenvalue = rayleigh_quotient(v_curr, h)
 
-# Print the calculated eigenvalue
-print("Calculated Eigenvalue:", eigenvalue)
+if __name__ == "__main__":
+    adam(512)
+
