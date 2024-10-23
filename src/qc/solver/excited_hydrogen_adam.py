8a6d25427b1fa6da41c2c0f5260a5200af6a6788
Blazej Tez <blatez@student.pg.edu.pl>
2024-10-23 21:39:21 +0200

    WIP with excited_adam

--- src/qc/solver/excited_hydrogen_adam.py
+++ src/qc/solver/excited_hydrogen_adam.py
@@ -6,6 +6,7 @@ import portion as P
 import Praktyki.cut_box_3D as box
 import qc.hamiltonian.hamiltonian as H
 import qc.data.raster as raster
+from qc.data.util_cub import load_cub
 
 ALPHA = 0.28294212105225837470023780155114
 def gram_schmidt(vectors):
@@ -67,7 +68,7 @@ class GoalGradient():
             return self.Y.T.dot(x)
 
 
-def gradient_descent_constrained(goal_gradient, x0, lambd0, lr=0.001, tol=0.005, max_iter=1000):
+def gradient_descent_constrained(goal_gradient, x0, lambd0, lr_x=0.001, lr_lambda=0.1, tol=0.005, max_iter=1000):
     x = x0
     lambd = lambd0
     A = goal_gradient.hamiltonian
@@ -97,6 +98,9 @@ def gradient_descent_constrained(goal_gradient, x0, lambd0, lr=0.001, tol=0.005,
         grad_lambda = goal_gradient.gradient_lambda(x)
         prev_eigenvalue = goal_gradient.objective_function(x,A,lambd)
 
+        if grad_lambda is not None:
+            constraint_violation = cp.linalg.norm(goal_gradient.gradient_lambda(x))
+            print(f"Iteration: {i}, Constraint violation: {constraint_violation}")
 
         # Update biased first moment estimate for x
         m_x = beta1 * m_x + (1 - beta1) * grad_x
@@ -107,7 +111,7 @@ def gradient_descent_constrained(goal_gradient, x0, lambd0, lr=0.001, tol=0.005,
         # Compute bias-corrected second raw moment estimate for x
         v_hat_x = v_x / (1 - beta2 ** t)
         # Update x
-        x_new = x - lr * m_hat_x / (cp.sqrt(v_hat_x) + epsilon)
+        x_new = x - lr_x * m_hat_x / (cp.sqrt(v_hat_x) + epsilon)
 
         if grad_lambda is not None:
             # Update biased first moment estimate for lambda
@@ -119,7 +123,7 @@ def gradient_descent_constrained(goal_gradient, x0, lambd0, lr=0.001, tol=0.005,
             # Compute bias-corrected second raw moment estimate for lambda
             v_hat_lambda = v_lambda / (1 - beta2 ** t)
             # Update lambda
-            lambd_new = lambd - lr * m_hat_lambda / (cp.sqrt(v_hat_lambda) + epsilon)
+            lambd_new = lambd + lr_lambda * m_hat_lambda / (cp.sqrt(v_hat_lambda) + epsilon)
         else:
             lambd_new = lambd
 
@@ -149,12 +153,13 @@ def gradient_descent_constrained(goal_gradient, x0, lambd0, lr=0.001, tol=0.005,
 
     return x, lambd
 
-def find_lowest_eigenvalues(A, initial_x, num_eigenvalues=5, lr=1e-5, tol=1e-5, max_iter=1000):
-    eigenvectors = []
-    eigenvalues = []
+def find_lowest_eigenvalues(A, initial_x, num_eigenvalues=5, lr_x=1e-5, lr_lambda=0.1, tol=1e-5, max_iter=1000, initial_Y=None, initial_lambd=None):
+    eigenvectors = [initial_Y]
+    eigenvalues = [initial_lambd]
     x0 = initial_x
 
     for i in range(num_eigenvalues):
+
         # Initialize lambda (Lagrange multipliers) as zeros
         lambd0 = cp.zeros((len(eigenvectors), 1), dtype=cp.float32) if eigenvectors else cp.zeros((0, 1), dtype=cp.float32)
         Y = cp.hstack(eigenvectors) if eigenvectors else None
@@ -163,7 +168,7 @@ def find_lowest_eigenvalues(A, initial_x, num_eigenvalues=5, lr=1e-5, tol=1e-5, 
         goal_gradient = GoalGradient(A, x0, Y=Y)
 
         # Perform constrained gradient descent
-        x, lambd = gradient_descent_constrained(goal_gradient, x0, lambd0, lr=lr, tol=tol, max_iter=max_iter)
+        x, lambd = gradient_descent_constrained(goal_gradient, x0, lambd0, lr_x=lr_x, lr_lambda=lr_lambda, tol=tol, max_iter=max_iter)
 
         # Compute the eigenvalue
         eigenvalue = goal_gradient.objective_function(x, A, lambd)
@@ -204,17 +209,23 @@ N = len(xl) * len(yl) * len(zl)
 
 A = H.HamiltonianOperatorCuPy(xl, yl, zl, extent=HYDROGEN_RADIUS)
 
-X, Y, Z = cp.meshgrid(cp.array(xl), cp.array(yl), cp.array(zl))
-gaussian_orbital = cp.exp(-ALPHA * (X ** 2 + Y ** 2 + Z ** 2), dtype=cp.float32)
-norm_factor = cp.sum(gaussian_orbital ** 2) * A.h ** 3
-gaussian_orbital /= cp.sqrt(norm_factor)
+# X, Y, Z = cp.meshgrid(cp.array(xl), cp.array(yl), cp.array(zl))
+# gaussian_orbital = cp.exp(-ALPHA * (X ** 2 + Y ** 2 + Z ** 2), dtype=cp.float32)
+# norm_factor = cp.sum(gaussian_orbital ** 2) * A.h ** 3
+# gaussian_orbital /= cp.sqrt(norm_factor)
 
-v_init = cp.reshape(gaussian_orbital, (len(X) * len(Y) * len(Z), 1))
+# v_init = cp.reshape(gaussian_orbital, (len(X) * len(Y) * len(Z), 1))
+v_init = cp.random.random((N, 1))
 goal_gradient = GoalGradient(hamiltonian=A, x=v_init)
+
+Y = load_cub("h100.cub")
+Y = cp.reshape(Y, (N, 1), )
+lambd = cp.asarray([[-0.49654406]], dtype=cp.float32)
 
 # Find the lowest five eigenvalues
 start_time = time.time()
-eigenvalues, eigenvectors = find_lowest_eigenvalues(A, v_init, num_eigenvalues=5, lr = 1e-4, tol=1e-4, max_iter = 7000)
+eigenvalues, eigenvectors = find_lowest_eigenvalues(A, v_init, num_eigenvalues=5, lr_x=1e-6, lr_lambda=1e-2,
+                                                    tol=1e-10, max_iter=7000, initial_Y=Y, initial_lambd=lambd)
 end_time = time.time()
 
 # Display the results
